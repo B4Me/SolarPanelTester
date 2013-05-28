45778b8fa6ad1e116b822e70a53a5c968b441fcb
b4me <holbbs@gmail.com>
2013-05-28 23:45:25 +0200

    concept loop made Missing.. a lot!

--- controller.c
+++ controller.c
@@ -11,6 +11,10 @@
 #include <complex.h>
 #include <stdbool.h>
 
+#define 	MAX_GATE_SUPPLY		3.3
+
+void sleep_ms();
+
 int main(int argc, char ** argv) {
 	//init DAC
 	//install DAC module
@@ -18,8 +22,7 @@ int main(int argc, char ** argv) {
 
 	//init ADC
 	//install ADC module
-	system(
-			"insmod /lib/modules/3.2.33-psp26.1/kernel/drivers/input/touchscreen/ti_tscadc.ko");
+	system("insmod /lib/modules/3.2.33-psp26.1/kernel/drivers/input/touchscreen/ti_tscadc.ko");
 	//Usage: cat /sys/devices/platform/omap/tsc/ain1
 
 	//init GPIO
@@ -57,19 +60,33 @@ int main(int argc, char ** argv) {
 		 * -update webpage
 		 * -wait for next test to start
 		 */
-a little horse
+
 		printf("Start. \n");
 		printf("Generate webpage and wait for user input. \n");
 		printf("Turn on halogen spots. \n");
 		printf("wait x sec. for lights to stabilize. \n");
 		printf("Setup DAC. \n");
 		printf("Test loop... \n");
+		for(float DAC_value = 0; DAC_value < MAX_GATE_SUPPLY; DAC_value += 0.01)
+		{
+			//write to DAC
+			system("%f", DAC_value);
+			//wait xx ms
+			sleep_ms(5);
+			//read ADC's
+			cat /sys/devices/platform/omap/tsc/ain1
+		}
 		printf("Turn lights off. \n");
 		printf("Calculate MPP. \n");
 		printf("Generate Test report/graphs. \n");
 		printf("update webpage. \n");
 		printf("wait for next test to start. \n");
+		sleep(1);
 	}
 	return 0;
 }
 
+void sleep_ms(int ms) {
+	usleep(ms * 1000); //convert to microseconds
+	return;
+}
